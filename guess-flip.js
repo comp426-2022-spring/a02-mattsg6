33d13ed73762051e8d6688dec076dc7da576a030
Matthew Gilmore <mattsg@email.unc.edu>
2022-02-10 22:38:36 -0500

    don't think it's my problem

--- guess-flip.js
+++ guess-flip.js
@@ -7,7 +7,7 @@ const args = minimist(process.argv.slice(2), {
 if(args.call == 'heads' || args.call == 'tails'){
     console.log(flipACoin(args.call));   
 }
-else if(args.call == ''){
+else if(args.call == null || args.call == ''){
     console.error('Error: no input\nUsage: node guess-flip --call=[heads|tails]');
 }
 else{
